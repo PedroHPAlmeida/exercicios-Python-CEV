df571eef9bd7d7794f2b92fab9bb9dbcf43f3822
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-09 13:40:47 -0300

    Maior e menor valores

---
+++ ex033.py
@@ -0,0 +1,20 @@
+#entrada
+n1 = int(input('Digite o 1º número: '))
+n2 = int(input('Digite o 2º número: '))
+n3 = int(input('Digite o 3º número: '))
+
+#processamento
+maior = menor = n1
+
+if n2 > maior:
+    maior = n2
+if n3 > maior:
+    maior = n3
+
+if n2 < menor:
+    menor = n2
+if n3 < menor:
+    menor = n3
+
+#saida
+print(f'Maior: {maior}\nMenor: {menor}')

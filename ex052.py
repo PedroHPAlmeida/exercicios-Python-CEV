102b8bdca81caa0dfb6f3a60824d82848d677087
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-25 17:46:34 -0300

    Números primos

---
+++ ex052.py
@@ -0,0 +1,18 @@
+#entrada
+n = int(input('Digite um número: '))
+
+#processamento
+divisivel = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        print(f'\033[33m{i}\033[m', end=' ')
+        divisivel += 1 
+    else: 
+        print(f'\033[31m{i}\033[m', end=' ')
+
+#saida
+print(f'\nO número {n} foi divisível {divisivel} vezes')
+if divisivel > 2:
+    print('E por isso ele NÃO É PRIMO!')
+else:
+    print('E por isso ele É PRIMO!')

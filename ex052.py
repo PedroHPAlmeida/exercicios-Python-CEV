1f0086841dcd43c7ca02eb0ac91504852398e875
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-08-25 17:48:37 -0300

    Números primos

--- ex052.py
+++ ex052.py
@@ -11,8 +11,8 @@ for i in range(1, n + 1):
         print(f'\033[31m{i}\033[m', end=' ')
 
 #saida
-print(f'\nO número {n} foi divisível {divisivel} vezes')
-if divisivel > 2:
-    print('E por isso ele NÃO É PRIMO!')
-else:
+print(f'\nO número {n} foi divisível {divisivel} vez(es)')
+if divisivel == 2:
     print('E por isso ele É PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
